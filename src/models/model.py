659ce2491dc1e8d734f6bc72ee963a3bde4f0edb
antoine311200 <antoine311200@gmail.com>
2024-03-01 21:57:06 +0100

    Model pipeline encoder + decoder and complete forward method using transfer style with graph cuts

--- src/models/model.py
+++ src/models/model.py
@@ -1,37 +1,45 @@
 import torch
 from torch import nn
 
-from .encoder import Encoder
-from .decoder import Decoder
-from ..energy import style_transfer
-from ..loss import ContentLoss, StyleLoss
+from src.models.encoder import Encoder
+from src.models.decoder import Decoder
+
+# import sys
+# sys.path.append("..")
+from src.energy import style_transfer
+from src.loss import ContentLoss, StyleLoss
 
 class TransferModel(nn.Module):
     def __init__(
         self,
         base_model = None,
-        blocks: list[int] = [0, 4, 11, 18, 31],
+        blocks: list[int] = [0, 2, 7, 12, 21],
         n_clusters: int = 3,
         alpha: float = 0.6,
-        gamma: float = 0.1,
+        lambd: float = 0.1,
+        gamma: float = 0.01,
         device="cpu",
     ):
         super(TransferModel, self).__init__()
-        self.encoder = Encoder(base_model, blocks, device)
-        self.decoder = Decoder()
+        self.encoder = Encoder(base_model, blocks=blocks, device=device)
+        self.decoder = Decoder(base_model, stop_layer=blocks[-1], device=device)
 
         self.n_clusters = n_clusters
         self.alpha = alpha
         self.gamma = gamma
+        self.lambd = lambd
 
         self.content_loss = ContentLoss()
         self.style_loss = StyleLoss()
+
+        self.device = device
 
     def forward(self, content_images, style_images, output_image=False):
         content_features = self.encoder(content_images)
         style_features = self.encoder(style_images)
 
         transfered_features = self.transfer(content_features, style_features)
+        print(transfered_features.shape)
         decoded_features = self.decoder(transfered_features)
 
         if output_image:
@@ -43,7 +51,7 @@ class TransferModel(nn.Module):
 
         content_loss = self.content_loss(encoded_features, content_features)
         style_loss = self.style_loss(all_encoded_features, all_style_features)
-        loss = content_loss + style_loss
+        loss = content_loss + self.gamma*style_loss
 
         return loss
 
@@ -57,6 +65,6 @@ class TransferModel(nn.Module):
                 style_features[i].detach().cpu().numpy(),
                 self.alpha,
                 self.n_clusters,
-                self.gamma,
+                self.lambd,
             )
         return transfered_features
