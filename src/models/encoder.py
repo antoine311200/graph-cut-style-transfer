e685493f6d1c854bf5c14577a4dfa4dd9b1215b9
ThomasLEMERCIER <t.lemercier27@gmail.com>
2024-03-26 21:40:28 +0100

    remove norm

--- src/models/encoder.py
+++ src/models/encoder.py
@@ -2,27 +2,6 @@ import torch
 from torch import nn
 from torchvision.models import vgg19, VGG19_Weights
 
-cnn_normalization_mean = torch.tensor([0.485, 0.456, 0.406])
-cnn_normalization_std = torch.tensor([0.229, 0.224, 0.225])
-
-class Normalization(nn.Module):
-    """Normalize a tensor image with mean and standard deviation."""
-
-    def __init__(self):
-        super(Normalization, self).__init__()
-        # .view the mean and std to make them [C x 1 x 1] so that they can
-        # directly work with image Tensor of shape [B x C x H x W].
-        # B is batch size. C is number of channels. H is height and W is width.
-        self.mean = cnn_normalization_mean.clone().detach().view(-1, 1, 1)
-        self.std = cnn_normalization_std.clone().detach().view(-1, 1, 1)
-
-        self.mean = nn.Parameter(self.mean, requires_grad=False)
-        self.std = nn.Parameter(self.std, requires_grad=False)
-
-    def forward(self, img):
-        # normalize ``img``
-        return (img - self.mean) / self.std
-
 class Encoder(nn.Module):
     """Encoder model for the Multimodal Style Transfer model."""
 
@@ -32,7 +11,6 @@ class Encoder(nn.Module):
         blocks: list[int] = [0, 4, 11, 18, 31],
     ):
         super(Encoder, self).__init__()
-        self.normalization = Normalization()
 
         if base_model is None:
             base_model = vgg19(weights=VGG19_Weights.DEFAULT)
@@ -83,7 +61,6 @@ class Encoder(nn.Module):
         Returns:
             torch.Tensor: Encoded tensor
         """
-        x = self.normalization(x)
         features = []
         for layer in self.block_layers:
             x = layer(x)
